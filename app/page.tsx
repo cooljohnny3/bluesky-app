1d41d2ae8f296886edbf950462d83107df3be153
John Brehm <cooljohnny3@gmail.com>
2024-11-11 10:07:18 -0800

    Factor out postSection component

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,9 @@
-'use client';
-
-import createPost from '@/actions/create-post';
-import { useState } from 'react';
+import PostSection from './postSection';
 
 export default function Home() {
-  const [loading, setLoading] = useState(false);
-  const [text, setText] = useState('');
-
   return (
     <main className="mt-4 flex flex-col gap-4 items-center">
-      <div className="w-full max-w-lg flex flex-col justify-center items-center gap-4">
-        <textarea
-          className="w-4/5 border border-black"
-          rows={4}
-          value={text}
-          onChange={(newText) => setText(newText.target.value)}
-        />
-        <button
-          className="px-2 py-1 bg-neutral-300 border border-black rounded-sm cursor-pointer hover:bg-neutral-400"
-          onClick={async () => {
-            setLoading(true);
-            try {
-              await createPost({ text });
-              setText('');
-            } finally {
-              setLoading(false);
-            }
-          }}
-          disabled={loading}
-        >
-          Submit
-        </button>
-      </div>
+      <PostSection className={'w-full max-w-lg'} />
     </main>
   );
 }
